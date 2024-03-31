63bad047b3a3c542c99e439ddd91d4685e67d685
ThanhCong <doanthanhcong17@gmail.com>
2024-03-31 21:41:24 +0700

    + search bar & vertical listview category

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,13 @@
 import 'package:e_commerce_app/common/widgets/customs_shapes/containers/primary_header_container.dart';
+import 'package:e_commerce_app/common/widgets/customs_shapes/containers/search_container.dart';
+import 'package:e_commerce_app/common/widgets/images_text_widgets/vertical_image_text.dart';
+import 'package:e_commerce_app/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce_app/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:e_commerce_app/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -17,9 +25,32 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   ///-- Appbar
                   THomeAppBar(),
+                  SizedBox(height: TSizes.spaceBtwSections),
 
                   ///-- SearchBar
+                  TSearchContainer(text: 'Search in Store'),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
                   ///-- Categories
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        ///-- Category Heading
+                        TSectionHeading(
+                          title: 'Popular Categories',
+                          showActionButton: false,
+                          textColor: TColors.white,
+                        ),
+                        SizedBox(
+                          height: TSizes.spaceBtwItems,
+                        ),
+
+                        ///-- Categories list view
+                        THomeCategories()
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
